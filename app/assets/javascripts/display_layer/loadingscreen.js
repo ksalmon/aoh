8e22bcdeb7d4e21b168b793f30e370149391595b
ksalmon <kyleraysalmon@gmail.com>
2014-11-22 23:02:12 -0500

    icons

---
+++ app/assets/javascripts/display_layer/loadingscreen.js
@@ -0,0 +1,23 @@
+//Loading Screen
+function onReady(callback) {
+    var intervalID = window.setInterval(checkReady, 1000);
+
+    function checkReady() {
+        if (document.getElementsByTagName('body')[0] !== undefined) {
+            window.clearInterval(intervalID);
+            callback.call(this);
+        }
+    }
+}
+
+function show(id, value) {
+    document.getElementById(id).style.display = value ? 'block' : 'none';
+}
+
+onReady(function () {
+    show('homepage', true);
+    show('loading', false).fadeOut(1500);
+});
+
+
+$(document).foundation();
