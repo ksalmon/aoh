34e027845780450c9af0393d64ef22a524654482
ksalmon <kyleraysalmon@gmail.com>
2014-11-24 15:57:33 -0500

    compile assets

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Aoh
   class Application < Rails::Application
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
