e244a4fc1c574a36f0e6847ca3552e35ea4df936
ksalmon <kyleraysalmon@gmail.com>
2014-11-30 12:00:15 -0500

    images

--- app/assets/javascripts/display_layer/loadingscreen.js
+++ app/assets/javascripts/display_layer/loadingscreen.js
@@ -1,23 +1,6 @@
 //Loading Screen
-function onReady(callback) {
-    var intervalID = window.setInterval(checkReady, 1000);
-
-    function checkReady() {
-        if (document.getElementsByTagName('body')[0] !== undefined) {
-            window.clearInterval(intervalID);
-            callback.call(this);
-        }
-    }
-}
-
-function show(id, value) {
-    document.getElementById(id).style.display = value ? 'block' : 'none';
-}
-
-onReady(function () {
-    show('homepage', true);
-    show('loading', false);
-});
-
+$(window).load(function() {
+  $("#loading").fadeOut("slow");
+})
 
 $(document).foundation();
